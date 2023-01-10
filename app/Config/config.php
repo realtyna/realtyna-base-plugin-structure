26bdeea6ec8ed3218b03b37b39ef31d4e91cec23
Chandler P <chandler.p@realtyna.net>
2023-01-10 14:50:11 -0500

    update config file

--- app/Config/config.php
+++ app/Config/config.php
@@ -5,25 +5,23 @@
  */
 return [
 
-    'namespace' => 'RealtynaAPIv4',
+    'namespace' => 'MustRename',
     'version' => '1.0.0',
     'api' => [
-        'namespace' => 'home-valuation'
+        'namespace' => 'must-rename'
     ],
     'path' => [
         'plugin-file-path'  => __DIR__ . '/../../plugin.php',
         'validator-messages' => __DIR__ . '/../../assets/langs/validation.php',
         'phinx' => [
-            'conf'  => __DIR__ . '/../Config/phinx.php'
+            'conf'  => __DIR__ . '/phinx.php'
         ],
         'assets' => [
-            'css' => __DIR__ . '/../../assets/css',
-            'js' => __DIR__ . '/../../assets/js',
+            'css' => plugin_dir_url(__DIR__ . '/../../plugin.php') . '/assets/css',
+            'js' => plugin_dir_url(__DIR__ . '/../../plugin.php') . '/assets/js',
         ],
         'plugin_dir' => __DIR__ . '/../../',
-        'views_dir' => __DIR__ . '/../../templates',
+        'view' => __DIR__ . '/../../templates',
     ],
     'jwt_secret' => REALTYNA_JWT_SECRET,
-
-
 ];
