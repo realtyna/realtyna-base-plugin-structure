094f1b948e8ed375888ae89ab5235974e8348078
Chandler P <chandler.p@realtyna.net>
2023-02-06 09:15:24 -0500

    define REALTYNA JWT SECRET before anything

--- plugin.php
+++ plugin.php
@@ -13,6 +13,32 @@ Domain Path: /assets/langs
 Requires PHP: 7.4
 */
 
+if (!defined('REALTYNA_JWT_SECRET')) {
+	$html = '<div class="notice notice-error"><p>
+                    <strong>REALTYNA_JWT_SECRET</strong> is not defined in <strong>wp-config.php</strong>.
+                    We will define a token for you but keep it in mind for better security you need 
+                    to define it in <strong>wp-config.php</strong> like so:
+<pre>
+define("REALTYNA_JWT_SECRET", "YOUR RANDOM SECRET TOKEN")                    
+</pre>
+                    (Token can be anything, example: ' . bin2hex(random_bytes(18)) . ')
+                </p></div>';
+	add_action('admin_notices', function () use($html) {
+		echo $html;
+	});
+	// Create a raw binary sha256 hash and base64 encode it.
+	$hash_base64 = base64_encode(hash('sha256', $_SERVER['SERVER_NAME'], true));
+	// Replace non-urlsafe chars to make the string urlsafe.
+	$hash_urlsafe = strtr($hash_base64, '+/', '-_');
+	// Trim base64 padding characters from the end.
+	$hash_urlsafe = rtrim($hash_urlsafe, '=');
+	// Shorten the string before returning.
+	$hash = substr($hash_urlsafe, 0, 50);
+
+	define('REALTYNA_JWT_SECRET', $hash);
+}
+
+
 if(!class_exists('Realtyna\MvcCore\StartUp')){
 	add_action('admin_notices', function (){
 		echo  '<div class="notice notice-error">';
